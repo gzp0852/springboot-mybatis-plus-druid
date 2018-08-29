3fd1ed165d57e304f2b715c33dc594caa236bc9a
gzp0852 <1037622823@qq.com>
2018-08-29 14:39:51 +0800

    a

--- PBC-AML/src/main/java/com/wistronits/aml/AmlApplication.java
+++ PBC-AML/src/main/java/com/wistronits/aml/AmlApplication.java
@@ -1,8 +1,5 @@
 package com.wistronits.aml;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wistronits.aml.filter.CustomFilter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +10,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -105,18 +101,6 @@ public class AmlApplication {
 	@Bean
 	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory)
 	{
-		/*Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
-		ObjectMapper om = new ObjectMapper();
-		om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-		om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-		jackson2JsonRedisSerializer.setObjectMapper(om);
-		RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
-		template.setConnectionFactory(redisConnectionFactory);
-		template.setKeySerializer(jackson2JsonRedisSerializer);
-		template.setValueSerializer(jackson2JsonRedisSerializer);
-		template.setHashKeySerializer(jackson2JsonRedisSerializer);
-		template.setHashValueSerializer(jackson2JsonRedisSerializer);
-		template.afterPropertiesSet();*/
 		RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
 		RedisSerializer<String> stringSerializer = new StringRedisSerializer();
 		template.setConnectionFactory(redisConnectionFactory);
